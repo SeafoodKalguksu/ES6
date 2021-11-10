ab8ce67941e856f2b1a5db14bc7a20abffe91bae
SeafoodKalguksu <heeseok.chong@icloud.com>
2021-11-10 13:16:32 +0900

    + findIndex()

--- array.js
+++ array.js
@@ -1,3 +1,10 @@
+const inventory = [
+    { name: 'apples', quantity: 2 },
+    { name: 'cherries', quantity: 5 },
+];
+
+let numbers = [1, 90, 78, 3, 22, 187, 21];
+
 // Array.prototype.forEach()
 // The forEach() executes a provided function once for each array element.
 // forEach((element, index, array) => { ... })
@@ -9,14 +16,7 @@ const logElements = (array) => array.forEach((element) => console.log(element));
 // that satisfies the provided testing function.
 // If no values satisfy the testing function, undefined is returned.
 // find((element, index, array) => { ... } )
-// Return value: The value of the first element in the array
-// that satisfies the provided testing function. Otherwise, undefined is returned.
-
 // Using arrow function and destructuring
-const inventory = [
-    { name: 'apples', quantity: 2 },
-    { name: 'cherries', quantity: 5 },
-];
 const result = inventory.find(({ name }) => name === 'cherries');
 console.log(result); // { name: 'cherries', quantity: 5 }
 
@@ -34,3 +34,12 @@ function isPrime(element, index, array) {
 
 console.log([4, 6, 8, 12].find(isPrime));
 console.log([4, 5, 8, 12].find(isPrime));
+
+// Array.prototype.findIndex()
+// The findIndex() returns the index of the first element in the array
+// that satisfies the provided testing function.
+// Otherwise, it returns -1, indicating that no element passed the test.
+// findIndex((element, index, array) => { ... } )
+const isGreaterThan100 = (element) => element > 100;
+console.log(numbers.findIndex(isGreaterThan100));
+// expected output: 3
