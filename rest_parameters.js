994d6880c81ddcb88bc63d138f4cd341e30790be
SeafoodKalguksu <heeseok.chong@icloud.com>
2021-11-10 21:07:53 +0900

    changed from test codes to example function

--- rest_parameters.js
+++ rest_parameters.js
@@ -1,14 +1,13 @@
-// The arguments object
-// arguments is an Array-like object accessible inside functions
-// that contains the values of the arguments passed to that function.
-// The arguments object is a local variable available within all non-arrow functions.
+function argumentsExample() {
+    // The arguments object
+    // arguments is an Array-like object accessible inside functions
+    // that contains the values of the arguments passed to that function.
+    // The arguments object is a local variable available within all non-arrow functions.
 
-// Note: The arguments object is not an Array.
-// “Array-like” means that arguments has a length property and
-// properties indexed from zero, but it doesn't have Array's built-in methods
-// like forEach() or map().
-
-function testArgumentsObject() {
+    // Note: The arguments object is not an Array.
+    // “Array-like” means that arguments has a length property and
+    // properties indexed from zero, but it doesn't have Array's built-in methods
+    // like forEach() or map().
     let number = arguments.length;
 
     switch (number) {
@@ -36,34 +35,36 @@ function testArgumentsObject() {
     }
 }
 
-testArgumentsObject();
-testArgumentsObject(1);
-testArgumentsObject(1, '2');
-testArgumentsObject(1, '2', 'hi');
+function restParametersExample() {
+    // Rest parameters
+    // The rest parameter syntax allows a function to accept
+    // an indefinite number of arguments as an array,
+    // ES6+: rest parameters should be preferred.
 
-// Rest parameters
-// The rest parameter syntax allows a function to accept
-// an indefinite number of arguments as an array,
-// ES6+: rest parameters should be preferred.
+    let add = (...args) => {
+        let sum = 0;
 
-let add = (...args) => {
+        for (let arg of args) {
+            sum += arg;
+        }
+
+        return sum;
+    };
+
     let sum = 0;
+    sum = add(3, 4, 5, 6);
+    sum = add.apply(null, [2, 3, 4, 5]);
+    console.log(sum);
 
-    for (let arg of args) {
-        sum += arg;
-    }
+    // min/max number in an array
+    const numbers = [5, 6, 2, 3, 7];
 
-    return sum;
-};
+    // using Math.min/Math.max apply
+    let max = Math.max.apply(null, numbers); // let max = Math.max(5, 6, 2, 3, 7);
+    console.log(max);
+}
 
-let sum = 0;
-sum = add(3, 4, 5, 6);
-sum = add.apply(null, [2, 3, 4, 5]);
-console.log(sum);
-
-// min/max number in an array
-const numbers = [5, 6, 2, 3, 7];
-
-// using Math.min/Math.max apply
-let max = Math.max.apply(null, numbers); // let max = Math.max(5, 6, 2, 3, 7);
-console.log(max);
+argumentsExample();
+argumentsExample(1);
+argumentsExample(1, '2');
+argumentsExample(1, '2', 'hi');
