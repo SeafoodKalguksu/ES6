b14149fd4791b140c6f72419a8c8d137d94bc3a3
SeafoodKalguksu <heeseok.chong@icloud.com>
2021-11-12 20:51:22 +0900

    + methodDefinition() and accessingProperties()

--- object.js
+++ object.js
@@ -14,11 +14,24 @@ function declaration() {
         console.log(favoriteFood);
     }
 
-    let object = {
-        property: function (parameters) {},
-        get property() {},
-        set property(value) {},
-    };
+    // A property of an object can also refer to a function or a getter or setter method.
+    function methodDefinition() {
+        let object = {
+            // property: function (parameters) {}, // ES5 style
+            property(parameters) {}, // ES6+ style
+            get property() {},
+            set property(value) {},
+        };
+
+        // In ES6, There is a way to concisely define properties whose values are generator functions:
+        function useGenerator() {
+            let object = {
+                *generator() {
+                    //   ...........
+                },
+            };
+        }
+    }
 }
 
 function accessingProperties() {
