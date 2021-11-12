f7cf59d687971300f3b055b8048fd7276f15c3ee
SeafoodKalguksu <heeseok.chong@icloud.com>
2021-11-12 20:01:52 +0900

    + declaration() and renameVariables()

--- destructuring.js
+++ destructuring.js
@@ -2,7 +2,7 @@
 // ad hoc packages of data.
 
 // Array destructuring
-function array() {
+const array = () => {
     let [first, second, third, rest] = [1, 2, 3, 4, 5]; // [1, 2, 3, 4]
 
     // Assigning the rest of an array to a variable
@@ -17,24 +17,30 @@ function array() {
     // Parsing an array returned from a function
     const returnArray = () => [1, 2, 3];
     [first, , third] = returnArray(); // Ignoring some returned values
-}
+};
 
 // Object destructuring
-function obj() {
+const obj = () => {
     const user = {
         id: 42,
         isVerified: true,
     };
     const { id, isVerified } = user;
 
-    let a, b;
-    ({ a, b } = { a: 1, b: 2 });
-    // {a, b} = {a: 1, b: 2} is not valid stand-alone syntax, as the {a, b}
-    // on the left-hand side is considered a block and not an object literal.
-}
+    // Declaration
+    const declaration = () => {
+        let { a, b } = { a: 1, b: 2 };
+
+        // Assignment separate from declaration
+        let c, d;
+        ({ c, d } = { c: 1, d: 2 });
+        // {c, d} = {c: 1, d: 2} is not valid stand-alone syntax, as the {c, d}
+        // on the left-hand side is considered a block and not an object literal.
+    };
+};
 
 // Nested object destructuring
-function nestedObject() {
+const nestedObject = () => {
     const website = {
         url: 'https://google.com',
         alias: 'Google',
@@ -46,11 +52,10 @@ function nestedObject() {
         services: { mail },
     } = website;
     console.log(mail);
-}
-nestedObject();
+};
 
 // Combined Array and Object Destructuring
-function combinedArrayAndObject() {
+const combinedArrayAndObject = () => {
     const props = [
         { id: 1, name: 'Fizz' },
         { id: 2, name: 'Buzz' },
@@ -58,11 +63,10 @@ function combinedArrayAndObject() {
     ];
     const [, , { name }] = props;
     console.log(name);
-}
-combinedArrayAndObject();
+};
 
 // Nested object and array destructuring
-function nestedObjectAndArray() {
+const nestedObjectAndArray = () => {
     const metadata = {
         title: 'Scratchpad',
         translations: [
@@ -77,22 +81,22 @@ function nestedObjectAndArray() {
         url: '/en-US/docs/Tools/Scratchpad',
     };
 
-    let {
-        title: englishTitle, // rename
-        translations: [
-            {
-                title: localeTitle, // rename
-            },
-        ],
-    } = metadata;
-
-    console.log(englishTitle); // "Scratchpad"
-    console.log(localeTitle); // "JavaScript-Umgebung"
-}
-nestedObjectAndArray();
+    // Rename to avoid the collision between variables.
+    const renameVariables = () => {
+        let {
+            /*
+            title, // 'Scratchpad'
+            translations: [{ title }], // 'JavaScript-Umgebung'
+            */
+            title: originalTitle,
+            translations: [{ title: localeTitle }],
+        } = metadata;
+        // console.log(title); // Collision occurred
+    };
+};
 
 // For of iteration and destructuring
-function for_of() {
+const for_of = () => {
     const people = [
         {
             name: 'Mike Smith',
@@ -120,5 +124,4 @@ function for_of() {
     } of people) {
         console.log(`${son}'s father is ${dad}.`);
     }
-}
-for_of();
+};
