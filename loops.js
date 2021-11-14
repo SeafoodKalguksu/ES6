8e72d77d6a910ce2a6ddc7b796bc05dec2dfd4d0
SeafoodKalguksu <heeseok.chong@icloud.com>
2021-11-14 22:02:00 +0900

    + for (property in object)

--- loops.js
+++ loops.js
@@ -4,30 +4,28 @@
 // while - loops through a block of code while a specified condition is true
 // do/while - also loops through a block of code while a specified condition is true
 
+// The for...in statement iterates over all enumerable properties of an object
+// that are keyed by strings (ignoring ones keyed by Symbols), including
+// inherited enumerable properties.
 function for_in() {
-    // for...in
-    // for (perperty in object) {
+    // for (property in object) {
     //     statement
     // }
-    // The for...in statement iterates over all enumerable properties of an object
-    // that are keyed by strings (ignoring ones keyed by Symbols), including
-    // inherited enumerable properties.
     const object = { a: 1, b: 2, c: 3 };
     for (const property in object) {
         console.log(`${property}: ${object[property]}`);
     }
 }
 
+// The for...of statement creates a loop iterating over iterable objects,
+// including: built-in String, Array, array-like objects (e.g., arguments
+// or NodeList), TypedArray, Map, Set, and user-defined iterables.
+// It invokes a custom iteration hook with statements to be executed for
+// the value of each distinct property of the object.
 function for_of() {
-    // for...of
     // for (value of iterable) {
     //     statement
     // }
-    // The for...of statement creates a loop iterating over iterable objects,
-    // including: built-in String, Array, array-like objects (e.g., arguments
-    // or NodeList), TypedArray, Map, Set, and user-defined iterables.
-    // It invokes a custom iteration hook with statements to be executed for
-    // the value of each distinct property of the object.
     const iterable = [3, 5, 7];
     iterable.foo = 'hello';
 
