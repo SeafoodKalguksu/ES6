77b5ee13a34053a89db9677a2ac876ab21bef145
SeafoodKalguksu <heeseok.chong@icloud.com>
2021-11-16 22:56:40 +0900

    + slice() and splice()

--- array.js
+++ array.js
@@ -1,12 +1,14 @@
-const numbers = [1, 90, 78, 3, 22, 187, 21];
+global = {
+    numbers: [1, 90, 78, 3, 22, 187, 21],
+    fruits: ['Banana', 'Orange', 'Lemon', 'Apple', 'Mango'],
+};
 
 function forEach() {
     // Array.prototype.forEach()
     // The forEach() executes a provided function once for each array element.
     // forEach((element, index, array) => { ... })
     // Return value: undefined
-    const logElements = (array) =>
-        array.forEach((element) => console.log(element));
+    global.numbers.forEach((element) => console.log(element));
 }
 
 function includes() {
@@ -33,17 +35,18 @@ function at() {
     // allowing for positive and negative integers.
     // Negative integers count back from the last item in the array.
     // at(index)
-    const numbers = [5, 12, 8, 130, 44];
 
     // Using index in Array
-    console.log(`numbers[2] = ${numbers[2]}`);
-    console.log(`numbers[-1] = ${numbers[-1]}`);
+    console.log(`global.numbers[2] = ${global.numbers[2]}`);
+    console.log(`global.numbers[-1] = ${global.numbers[-1]}`);
 
     // Using at() method
-    console.log(`numbers.at(-1) = ${numbers.at(-1)}`);
+    console.log(`global.numbers.at(-1) = ${global.numbers.at(-1)}`);
 
     // Using slice() method. Note an array is returned
-    console.log(`numbers.slice(-2, -1) = ${numbers.slice(-3, -1)}`);
+    console.log(
+        `global.numbers.slice(-2, -1) = ${global.numbers.slice(-3, -1)}`
+    );
 }
 
 function find() {
@@ -83,7 +86,7 @@ function findIndex() {
     // Otherwise, it returns -1, indicating that no element passed the test.
     // findIndex((element, index, array) => { ... } )
     const isGreaterThan100 = (element) => element > 100;
-    console.log(numbers.findIndex(isGreaterThan100));
+    console.log(global.numbers.findIndex(isGreaterThan100));
 }
 
 function indexOf() {
@@ -101,7 +104,7 @@ function reverse() {
     // The first array element becomes the last,
     // and the last array element becomes the first.
     // Return value: The reversed array.
-    const reversedNumbers = numbers.reverse();
+    const reversedNumbers = global.numbers.reverse();
 }
 
 function sort() {
@@ -109,14 +112,14 @@ function sort() {
     // The sort() sorts the elements of an array in place and returns the sorted array.
     // The default sort order is ascending, built upon converting the elements into
     // strings, then comparing their sequences of UTF-16 code units values.
-    console.log(numbers); // [1, 90, 78, 3, 22, 187, 21];
-    console.log(numbers.sort()); // Ascending based on string
-    console.log(numbers.sort().reverse());
-    console.log(numbers.sort((a, b) => (a > b ? -1 : 1)));
+    console.log(global.numbers); // [1, 90, 78, 3, 22, 187, 21];
+    console.log(global.numbers.sort()); // Ascending based on string
+    console.log(global.numbers.sort().reverse());
+    console.log(global.numbers.sort((a, b) => (a > b ? -1 : 1)));
 
-    console.log(numbers.sort((a, b) => a - b)); // Ascending based on number
-    console.log(numbers.sort((a, b) => a - b).reverse());
-    console.log(numbers.sort((a, b) => b - a));
+    console.log(global.numbers.sort((a, b) => a - b)); // Ascending based on number
+    console.log(global.numbers.sort((a, b) => a - b).reverse());
+    console.log(global.numbers.sort((a, b) => b - a));
 
     const students = [{ name: 'Albert' }, { name: 'Bob' }, { name: 'Claudia' }];
     console.log(students);
@@ -158,7 +161,6 @@ function from() {
         )
     ); // ["A", ... , "Z"]
 }
-from();
 
 function join() {
     // Array.prototype.join()
@@ -178,24 +180,24 @@ function pushAndPop() {
     // Array.prototype.pop()
     // The pop() method removes the last element from an array and returns that element.
     // Return value: The removed element from the array; undefined if the array is empty.
-    const number = numbers && numbers.pop();
+    const number = global.numbers && global.numbers.pop();
 
     // Array.prototype.push()
     // The push() method adds one or more elements to the end of an array
     // and returns the new length of the array.
     // push(element0, element1, ... , elementN)
-    const newLength = numbers && numbers.push(number);
+    const newLength = global.numbers && global.numbers.push(number);
 }
 
 function shiftAndUnshift() {
     // Array.prototype.shift()
     // The shift() removes the first element from an array and returns that removed element.
-    const number = numbers && numbers.shift();
+    const number = global.numbers && global.numbers.shift();
     // Array.prototype.unshift()
     // The unshift() method adds one or more elements to the beginning of an array
     // and returns the new length of the array.
     // unshift(element0, element1, ... , elementN)
-    const newLength = numbers && numbers.unshift(number);
+    const newLength = global.numbers && global.numbers.unshift(number);
 }
 
 function concat() {
@@ -210,12 +212,42 @@ function concat() {
     // Nested arrays
     const num1 = [[1]];
     const num2 = [2, [3]];
-    const numbers = num1.concat(num2);
+    numbers = num1.concat(num2);
     // remove [] from num1 and num2: [[1]] => [1], [2, [3]] => 2, [3]
-    console.log(numbers); // [ [1], 2, [3] ] ]
+    console.log(global.numbers); // [ [1], 2, [3] ] ]
 
     num1[0].push(4);
     // num2[0].push(5); // 2.push(5) because num2[0] is not an array.
     num2[1].push(6); // [3].push(6)
-    console.log(numbers); // [ [ 1, 4 ], 2, [ 3, 6 ] ]
+    console.log(global.numbers); // [ [ 1, 4 ], 2, [ 3, 6 ] ]
+}
+
+// Array.prototype.slice(start, end)
+// The slice() method returns a shallow copy of a portion of an array into
+// a new array object selected from start to end (end not included) where start
+// and end represent the index of items in that array. The original array will
+// not be modified.
+// Return value: a new array containing the extracted elements.
+function slice() {
+    let fruits = global.fruits; // ['Banana', 'Orange', 'Lemon', 'Apple', 'Mango']
+    let citrus = fruits.slice(1, 3); // [ 'Orange', 'Lemon' ]
+}
+
+// Array.prototype.splice(start, deleteCount, item1, item2, itemN)
+// The splice() method changes the contents of an array by removing or replacing
+// existing elements and/or adding new elements in place. To access part of
+// an array without modifying it, see slice().
+// Return value: an array containing the deleted elements.
+function splice() {
+    let arr = global.numbers; // [1, 90, 78, 3, 22, 187, 21]
+
+    // To remove elements.
+    arr.splice(1, 2); // [1, 3, 22, 187, 21]
+
+    // To replace elements.
+    arr.splice(1, 3, 100, 200); // [ 1, 100, 200, 21 ]
+    arr.splice(1, 0, 31, 87); //  [1, 31, 87, 100, 200, 21 ]
+    let result = arr.splice(3, 2); // [100, 200]
+    console.log(result);
+    console.log(global.numbers);
 }
