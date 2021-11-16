14f096e0343a2e01c688246ba5bb6627436b24a5
SeafoodKalguksu <heeseok.chong@icloud.com>
2021-11-16 21:56:05 +0900

    + split() and iterate()

--- string.js
+++ string.js
@@ -1,17 +1,17 @@
-const globalStrings = {
-    desc: 'This is a description for string.',
-    text: 'Mozilla',
+const global = {
+    desc: 'This is a description for global object.',
+    company: 'Mozilla',
     name: 'Mike',
     greeting: '   Hello world!   ',
     fruits: ['01. Orange', '02. Apple', '03. Kiwi', '04. Pear'],
 };
 
 // Property
-const length = globalStrings.desc.length;
+const length = global.desc.length;
 
 // strings are immutable.
-globalStrings.name[3] = 'y'; // nothing happened.
-globalStrings.name = 'Miky'; // "Miky"
+global.name[3] = 'y'; // nothing happened.
+global.name = 'Miky'; // "Miky"
 
 // String.prototype.includes(searchString, position)
 // The includes() method performs a case-sensitive search to determine whether one string may be found within another string, returning true or false as appropriate.
@@ -31,11 +31,11 @@ function includes() {
 // the first occurrence of the specified value, starting the search at fromIndex.
 // Returns -1 if the value is not found.
 indexOf = () => {
-    globalStrings.desc.indexOf('is'); // return 2
+    global.desc.indexOf('is'); // return 2
 
-    // the if statement is false because return 0 from globalStrings.desc.indexOf('This')
-    // if (globalStrings.desc.indexOf('This')) {
-    if (globalStrings.desc.indexOf('This') > -1) {
+    // the if statement is false because return 0 from global.desc.indexOf('This')
+    // if (global.desc.indexOf('This')) {
+    if (global.desc.indexOf('This') > -1) {
         console.log("'That' is included in the given sentence.");
     }
 };
@@ -44,14 +44,14 @@ indexOf = () => {
 // The slice() extracts a section of a string and returns it as a new string,
 // without modifying the original string.
 slice = () => {
-    globalStrings.desc.slice(2); // "is is a description for string."
-    globalStrings.desc.slice(0, 5); // "This "
-    globalStrings.desc.slice(2, -2); // "is is a description for strin"
+    global.desc.slice(2); // "is is a description for string."
+    global.desc.slice(0, 5); // "This "
+    global.desc.slice(2, -2); // "is is a description for strin"
 
     // slice() also treats NaN arguments as 0, but when it is given negative
     // values it counts backwards from the end of the string to find the indexes.
-    console.log(globalStrings.text.slice(-5, 2)); // => ""
-    console.log(globalStrings.text.slice(-5, -2)); // => "zil"
+    console.log(global.company.slice(-5, 2)); // => ""
+    console.log(global.company.slice(-5, -2)); // => "zil"
 };
 
 // String.prototype.substring(indexStart, indexEnd)
@@ -59,46 +59,46 @@ slice = () => {
 // end indexes, or to the end of the string.
 // Return value: a new string containing the specified part of the given string.
 substring = () => {
-    console.log(globalStrings.text.substring(1, 3)); // "oz"
+    console.log(global.company.substring(1, 3)); // "oz"
 
     // If indexStart is greater than indexEnd, then the effect of substring() is
     //  as if the two arguments were swapped; See example below.
-    console.log(globalStrings.text.substring(3, 1)); // is equal to str.substring(1, 3).
+    console.log(global.company.substring(3, 1)); // is equal to str.substring(1, 3).
 
     // ***
     // Any argument value that is less than 0 or greater than stringName.
     // length is treated as if it were 0 and stringName.length, respectively.
     // ***
-    console.log(globalStrings.text.substring(-5, 2)); // => "Mo"
-    console.log(globalStrings.text.substring(-5, -2)); // => ""
+    console.log(global.company.substring(-5, 2)); // => "Mo"
+    console.log(global.company.substring(-5, -2)); // => ""
 };
 
 // String.prototype.substr(start, length)
 // The substr() returns a portion of the string, starting at the specified index
 //  and extending for a given number of characters afterwards.
 substr = () => {
-    console.log(globalStrings.text.substr(0, 1)); // 'M'
-    console.log(globalStrings.text.substr(1, 0)); // ''
-    console.log(globalStrings.text.substr(-1, 1)); // 'a'
-    console.log(globalStrings.text.substr(1, -1)); // ''
-    console.log(globalStrings.text.substr(-3)); // 'lla'
-    console.log(globalStrings.text.substr(1)); // 'ozilla'
-    console.log(globalStrings.text.substr(-20, 2)); // 'Mo'
-    console.log(globalStrings.text.substr(20, 2)); // ''
+    console.log(global.company.substr(0, 1)); // 'M'
+    console.log(global.company.substr(1, 0)); // ''
+    console.log(global.company.substr(-1, 1)); // 'a'
+    console.log(global.company.substr(1, -1)); // ''
+    console.log(global.company.substr(-3)); // 'lla'
+    console.log(global.company.substr(1)); // 'ozilla'
+    console.log(global.company.substr(-20, 2)); // 'Mo'
+    console.log(global.company.substr(20, 2)); // ''
 };
 
 // String.prototype.trim()
 // The trim() method removes whitespace from both ends of a string. Whitespace
 // in this context is all the whitespace characters (space, tab, no-break space,
 // etc.) and all the line terminator characters (LF, CR, etc.).
-trim = () => console.log(globalStrings.greeting.trim());
+trim = () => console.log(global.greeting.trim());
 // expected output: "   Hello world!   " => "Hello world!"
 
 // String.prototype.codePointAt(pos)
 // returns a non-negative integer that is the UTF-16 code point value.
 codePointAt = () => {
-    console.log(globalStrings.name.codePointAt()); // "M"
-    console.log(globalStrings.name.codePointAt(1)); // "i"
+    console.log(global.name.codePointAt()); // "M"
+    console.log(global.name.codePointAt(1)); // "i"
 };
 
 // String.fromCodePoint(num1, num2, ..., numN)
@@ -109,14 +109,12 @@ fromCodePoint = () => {
     console.log(String.fromCodePoint(65, 90)); // "AZ"
 };
 
-toUpperCase = () => globalStrings.desc.toUpperCase();
-toLowerCase = () => globalStrings.desc.toLowerCase();
+toUpperCase = () => global.desc.toUpperCase();
+toLowerCase = () => global.desc.toLowerCase();
 
 logFruitName = () => {
     let fruitNameList = [];
-    globalStrings.fruits.forEach((element) =>
-        fruitNameList.push(element.slice(4))
-    );
+    global.fruits.forEach((element) => fruitNameList.push(element.slice(4)));
     console.log(fruitNameList);
 };
 
@@ -143,3 +141,43 @@ checkForbiddenWord = (source = '', forbiddenWord = '') =>
     source.includes(forbiddenWord) ? true : false;
 
 checkForbiddenWord('Beach bitch Bitch Bitch Beachbitch beach bitch!', 'bitch');
+
+// String.prototype.split(separator, limit)
+// The split() divides a String into an ordered list of substrings, puts these
+// substrings into an array, and returns the array. The division is done by
+// searching for a pattern; where the pattern is provided as the first parameter
+// in the method's call.
+function split() {
+    const str = 'The quick brown fox jumps over the lazy dog.';
+
+    const words = str.split(' ');
+    console.log(words[3]);
+    // expected output: "fox"
+
+    const chars = str.split('');
+    console.log(chars[8]);
+    // expected output: "k"
+
+    const strCopy = str.split();
+    console.log(strCopy);
+    // expected output: Array ["The quick brown fox jumps over the lazy dog."]
+}
+
+// Iterate through string.
+function iterate() {
+    // ES6+
+    [...global.name].forEach((element) => console.log(element));
+
+    // ES5
+    global.name
+        .split('')
+        .forEach((element) => console.log(`str.split: ${element}`));
+    Array.from(global.name, (element) => console.log(`Array.from: ${element}`));
+    Object.assign([], global.name).forEach((element) =>
+        console.log(`Object.assign: ${element}`)
+    );
+
+    for (let str of global.name) {
+        console.log(`for of: ${str}`);
+    }
+}
