d2d1849368ebec93f6c795ee93a9462c4119dbd8
SeafoodKalguksu <heeseok.chong@icloud.com>
2021-11-18 22:32:29 +0900

    + using arrow function to make a closure

--- closure.js
+++ closure.js
@@ -1,6 +1,9 @@
 // Closures
 // A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). In other words, a closure gives you access to an outer function’s scope from an inner function. In JavaScript, closures are created every time a function is created, at function creation time.
 function closure() {
+    // Using arrow function
+    // const makeAdder = (x) => (y) => x + y;
+
     function makeAdder(x) {
         // the function (y) inner function is returned from the outer function before being executed.
         return function (y) {
@@ -8,15 +11,9 @@ function closure() {
         };
     }
 
-    // Using arrow function
-    // const makeAdder = (x) =>
-    //     function (y) {
-    //         return x + y;
-    //     };
-
-    // In essence, makeAdder is a function factory. It creates functions that can add a specific value to their argument. In the above example, the function factory creates two new functions—one that adds five to its argument, and one that adds 10.
     const add5 = makeAdder(5);
     const add10 = makeAdder(10);
+    // In essence, makeAdder is a function factory. It creates functions that can add a specific value to their argument. In the above example, the function factory creates two new functions—one that adds five to its argument, and one that adds 10.
     // add5 and add10 are both closures. They share the same function body definition, but store different lexical environments. In add5's lexical environment, x is 5, while in the lexical environment for add10, x is 10.
 
     console.log(add5(2));
