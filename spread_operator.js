f614bf345be8f1993b578459bcd2812493465357
SeafoodKalguksu <heeseok.chong@icloud.com>
2021-11-18 10:23:06 +0900

    + Differences between ES5 and ES6+

---
+++ spread_operator.js
@@ -0,0 +1,21 @@
+global = {
+    user: { name: 'Mike' },
+    info: { age: 30 },
+    fe: ['JavaScript', 'React'],
+    langs: ['Korean', 'English'],
+};
+
+// Differences between ES5 and ES6+
+function es5() {
+    // ES5 style
+    let user = Object.assign({}, global.user, info, { skills: [] });
+    fe.forEach((e) => user.skills.push(e));
+    langs.forEach((e) => user.skills.push(e));
+
+    // ES6+ style
+    user = {
+        ...global.user,
+        ...global.info,
+        skills: [...global.fe, ...global.langs],
+    };
+}
