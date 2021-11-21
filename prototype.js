da17647fe5aa2dc052ada845b606f40d79a4eb2d
SeafoodKalguksu <heeseok.chong@icloud.com>
2021-11-21 23:51:29 +0900

    + chain prototype by using constructor()

--- prototype.js
+++ prototype.js
@@ -1,20 +1,20 @@
-const car = {
-    color: 'white',
-    wheels: 4,
-    drive() {
-        console.log('drive...');
-    },
-};
-
-const luxurySedan = {
-    isExpensive: true,
-};
-
-const benz = {
-    color: 'black',
-};
-
 function prototypeChain() {
+    const car = {
+        color: 'white',
+        wheels: 4,
+        drive() {
+            console.log('drive...');
+        },
+    };
+
+    const luxurySedan = {
+        isExpensive: true,
+    };
+
+    const benz = {
+        color: 'black',
+    };
+
     // Object.prototype.__proto__
     // DEPRECATED. This is for example purposes only. DO NOT DO THIS in real code.
     luxurySedan.__proto__ = car;
@@ -29,6 +29,49 @@ function prototypeChain() {
 
     // Showing all keys including inherited keys from luxurySedan and car objects.
     for (let key in benz) {
+        console.log(key);
+    }
+}
+
+// --------------------------------------------------------------------
+// |Constructor of Bmw |                     |Prototype object of Bmw |
+// |-------------------|---------------------|------------------------|
+// |prototype          | ==================> |                        |
+// |-------------------|---------------------|------------------------|
+// |                   | <================== | constructor            |
+// --------------------------------------------------------------------
+//                                                  ⬇️⬇️⬇️(inheritance)
+//                                          instance of prototype object
+function usingConstructor() {
+    const Bmw = function (color) {
+        this.color = color;
+    };
+
+    // Option 1:
+    Bmw.prototype.wheels = 4;
+    Bmw.prototype.isExpensive = true;
+    Bmw.prototype.drive = function () {
+        console.log('drive...');
+    };
+
+    // Option 2:
+    // Bmw.prototype = {
+    //     constructor: Bmw,
+    //     wheels: 4,
+    //     isExpensive: true,
+    //     drive() {
+    //         console.log('drive...');
+    //     },
+    // };
+
+    const x5 = new Bmw('white');
+    console.log(x5); // own property only
+    console.log(x5.wheels); // accessing the property of the prototype of x5
+    x5.drive(); // execute the method of the prototype of x5
+    console.log(Bmw.isExpensive); // undefined
+    console.log(Bmw.prototype.isExpensive); // true
+    // Showing all properties
+    for (let key in x5) {
         console.log(key);
     }
 }
